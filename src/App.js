a7a1353f46074bfbaace6ff29ae3ebd1d8721be0
PaulEnt <pauljamesentwistle@gmail.com>
2022-07-13 15:46:18 +0100

    Installed and implemented faker to add names and prices

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import Cat from "./Cat";
+import faker from 'faker';
 
-function App() {
+const App = () => {
+
+  const [catPic, setCatPic] = useState([]);
+
+  useEffect(() => {
+    const getCatImages = async () => {
+        const response = await fetch("https://api.thecatapi.com/v1/images/search?size=small&limit=12");
+        const data = await response.json();
+        setCatPic(data);
+      };
+    getCatImages();
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Test</h1>
+    <div id="container">
+      <h1>CATS</h1>
+      <div id="card">
+        {catPic ? (
+          catPic.map((catItem, index) => {
+            return <Cat image={catItem.url} name={faker.name.firstName()} species={faker.animal.cat()} price={faker.commerce.price()} alt="Picture of cat" key={index} />;
+          })
+        ) : (
+          <h2>Loading...</h2>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
