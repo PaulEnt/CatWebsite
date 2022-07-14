78467edad800a9a6a0333690d2886ee82ffce82e
Fitz8 <liamfitzgerald@hotmail.co.uk>
2022-07-14 10:28:22 +0100

    Cats are now stored in a state when the page is rendered

--- src/Cat.js
+++ src/Cat.js
@@ -1,11 +1,11 @@
-const Cat = ({ name, image, species, alt, price }) => {
-    return (
-      <div>
-        <img src={image} alt={alt} />
-        <h2>{name}</h2>
-        <p>{species}</p>
-        <p>{price}</p>
-      </div>
-    );
-  };
-  export default Cat;
+const Cat = ({ name, image, species, alt, price, handleClick }) => {
+  return (
+    <div>
+      <img src={image} alt={alt} />
+      <h2>{name}</h2>
+      <p>{species}</p>
+      <p>{price}</p>
+    </div>
+  );
+};
+export default Cat;
