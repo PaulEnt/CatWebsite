6e6da62faad99ad099ff21c3793f4fb33e9d4bee
PaulEnt <pauljamesentwistle@gmail.com>
2022-07-14 16:36:53 +0100

    Co-authored-by: Lucas <LucasPriestley@users.noreply.github.com> Co-authored-by: liamfitzgerald@hotmail.co.uk <liamfitzgerald@hotmail.co.uk>

---
+++ src/Basket.js
@@ -0,0 +1,22 @@
+import BasketItem from './BasketItem';
+
+const Basket = ({ basket }) => {
+    return (
+      <div>
+        <h1>Basket</h1>
+        <ol>
+          {basket.map((item, index) => {
+            return (
+              <BasketItem
+                name={item.name}
+                price={item.price}
+                // handleClick={() => removeItem(index)}
+              />
+            )
+          })}
+        </ol>
+      </div>
+    )
+}
+
+export default Basket;
